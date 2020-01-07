384075ab1639ae9ef644011011fd7d82f7210d5c
Charlie Paterson <Charlieed.paterson@googlemail.com>
2020-01-07 15:32:54 +0000

    Tested and developed basic NoteListView object

--- spec/note-list-view-test.js
+++ spec/note-list-view-test.js
@@ -3,8 +3,9 @@ function returnsHTMLString(){
   noteList.createNote('note1');
   noteList.createNote('note2');
   noteList.createNote('note3');
+  noteList.createNote('hello world')
   var noteListView = new NoteListView(noteList);
-  assert.eq('<ul><li><div>note1</div></li><li><div>note2</div></li><li><div>note3</div></li></ul>', noteListView.htmlString());
+  assert.eq('<ul><li><div>note1</div></li><li><div>note2</div></li><li><div>note3</div></li><li><div>hello world</div></li></ul>', noteListView.htmlList());
 };
 
 returnsHTMLString();
